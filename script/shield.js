2078368abb0793a988267a79cf2390ee63c3cdd5
UnderCoverDemon77 <115486244+UnderCoverDemon77@users.noreply.github.com>
2022-11-21 00:43:04 +0000

    Add files via upload

---
+++ script/shield.js
@@ -0,0 +1,10 @@
+let win = 0
+let loss = 1
+let winLoss = winLossRatio(win, loss)
+function winLossRatio(win, loss){
+  return ((win/(win+loss))*100).toPrecision(3)+"%"
+}
+
+document.getElementById("win").innerHTML = win
+document.getElementById("loss").innerHTML = loss
+document.getElementById("winLoss").innerHTML = winLoss
